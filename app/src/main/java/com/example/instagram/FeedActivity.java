ee9b8629bd08efea09433d23d8fdf57849b41fbc
Eva Isha Prakash <eprakash@fb.com>
2021-07-08 16:14:39 -0700

    User can view the last 20 posts submitted to Instagram

---
+++ app/src/main/java/com/example/instagram/FeedActivity.java
@@ -0,0 +1,79 @@
+package com.example.instagram;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeedActivity extends AppCompatActivity {
+
+    private final String TAG = "FeedActivity";
+    private RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        rvPosts = findViewById(R.id.rvPosts);
+
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(this, allPosts);
+
+        // Set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+
+        // Set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(this));
+
+        // Query posts from Parstagram
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        // Specify what type of data we want to query - Post.class
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+
+        // Include data referred by user key
+        query.include(Post.KEY_USER);
+
+        // Limit query to latest 20 items
+        query.setLimit(20);
+
+        // Order posts by creation date (newest first)
+        query.addDescendingOrder("createdAt");
+
+        // Start an asynchronous call for posts
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                // Check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                // For debugging purposes, print every post description to logcat
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+
+                // Save received posts to list and notify adapter of new data
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+}
